136ae72718e401ae29a6d8b5a963b70bbc9b7355
Irist <152891430+j0hntran@users.noreply.github.com>
2024-03-07 19:30:52 +0700

    a

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Các chị em phụ nữ';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng các bạn';
+const contentCard = 'Chúc cho các bà mẹ, các bạn nữ có một ngày 8/3 thật ý nghĩa và vui vẻ bên người thân và bạn bè! ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'https://img.meta.com.vn/Data/image/2021/09/22/anh-meo-cute-trai-tim-6.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
